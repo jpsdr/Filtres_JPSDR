494f11a71f3ec9c5b4867e2d5d507486329f3b1c
jpsdr <jpsdr.psx@free.fr>
2020-04-27 21:55:15 +0200

    Fix resources, part 2.
    Signed-off-by: jpsdr <jpsdr.psx@free.fr>

---
+++ VHS_II/VHS_II_resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by JPSDR_VHS.rc
+//
+#define IDD_JPSDR_VHS                   151
+#define IDC_THRESHOLD_1                 1050
+#define IDC_THRESHOLD_2                 1051
+#define IDC_MODE_2                      1052
+#define IDC_MODE_3                      1053
+#define IDC_MODE_1                      1054
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        152
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1055
+#define _APS_NEXT_SYMED_VALUE           151
+#endif
+#endif
